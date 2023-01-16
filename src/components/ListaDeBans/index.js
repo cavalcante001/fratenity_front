4c159d495ff6dd10e77c25806efb8a3289b57661
Paulo <paulocavalcante001@outlook.com>
2023-01-15 23:42:40 -0300

    corrigindo paginação

--- src/components/ListaDeBans/index.js
+++ src/components/ListaDeBans/index.js
@@ -42,49 +42,27 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
-
 export const ListaDeBans = () => {
 
   const [listaBan, setListaBan] = useState([]);
   const [page, setPage] = React.useState(0);
+  const [totalPage, setTotalPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
   useEffect(() => {
     const bans = async () => {
-        setListaBan(await api.getAllBans(1));
+        const lista = await api.getAllBans(1)
+        setListaBan(lista.bans);
+        setTotalPage(lista.total_page);
     }
     bans();
   }, []);
 
-  const handleChangePage = (event, newPage) => {
+  const handleChangePage = async (event, newPage) => {
+    let lista = await api.getAllBans(newPage + 1)
+    setListaBan(lista.bans);
+    setTotalPage(lista.total_page);
     setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
   };
 
   return (
@@ -105,8 +83,8 @@ export const ListaDeBans = () => {
             </TableRow>
           </TableHead>
           <TableBody>            
-            {listaBan.bans &&
-                listaBan.bans.map((row, key) => {               
+            {listaBan &&
+                listaBan.map((row, key) => {               
                     return (
                     <StyledTableRow hover role="checkbox" tabIndex={-1} key={key}>
                         {columns.map((column) => {                           
@@ -130,11 +108,10 @@ export const ListaDeBans = () => {
       <TablePagination
         rowsPerPageOptions={[10]}
         component="div"
-        count={rows.length}
+        count={totalPage}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
       />
     </Paper>
   );
