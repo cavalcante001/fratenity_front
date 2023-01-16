035c175efc39a826e035c943918e4aaeb9c253b6
Paulo <paulocavalcante001@outlook.com>
2023-01-15 22:50:05 -0300

    alterado endpoint bans

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://18.233.65.91:5001/api/'
+    baseURL: 'https://www.fraternitygames.com.br/api/'
 });
 
 export const api = {
